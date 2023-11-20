0e75b2c14721734d35c00b90e3805b25ca262587
LampShade63 <151576075+LampShade63@users.noreply.github.com>
2023-11-20 15:52:21 -0500

    Update BasicCalculator.java

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -40,4 +40,53 @@ public class BasicCalculator extends JFrame
             BasicCalculator calculator = new BasicCalculator();
             calculator.setVisible(true);
     }
+    public static void main()
+    {
+       
+    }
+    public double add() //Allows the addition function
+    {
+	
+    }
+    public void subtract() //Allows the subtraction function
+    {
+	
+    }
+    public void squareRoot() //Allows the square root function
+    {
+
+    }
+
+    public void multiply () //Allows the multiplication function
+    {
+   
+    }
+   
+    public void divide() //Allows the division function
+    {
+       
+    }
+   
+    public void square() //Allows the square function
+    {
+
+
+    }
+    public void result() //Allows the equals function
+    {
+
+
+    }
+    public void clear() //Allows the calculator screen to be cleared
+    {
+
+
+    }
+    public void delete() //Allows a deletion from the screen
+    {
+	
+    }
+   
+}
+
 }
